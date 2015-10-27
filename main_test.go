4235fd1a7ed9f84a02df2ec6185a8fcc281cd0ac
rw <me@rwinslow.com>
2015-10-26 17:41:53 -0700

    track bandwidth in benchmarks. do fake work with `name`.

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 	flatbuffers "github.com/google/flatbuffers/go"
 )
@@ -11,16 +12,23 @@ func BenchmarkWrite(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N ; i++ {
 		builder.Reset()
-		MakeUser(builder, []byte("Arthur Dent"), 42)
+		buf := MakeUser(builder, []byte("Arthur Dent"), 42)
+		if i == 0 {
+			b.SetBytes(int64(len(buf)))
+		}
 	}
 }
 
 func BenchmarkRead(b *testing.B) {
 	builder := flatbuffers.NewBuilder(0)
-	buf := MakeUser(builder, []byte("Arthur Dent"), 42)
+	name := []byte("Arthur Dent")
+	buf := MakeUser(builder, name, 42)
+	b.SetBytes(int64(len(buf)))
 	b.ReportAllocs()
 	for i := 0; i < b.N ; i++ {
-		ReadUser(buf)
+		got_name, _ := ReadUser(buf)
+		// do some work to prevent cheating the benchmark:
+		bytes.Equal(got_name, name)
 	}
 }
 
@@ -29,7 +37,12 @@ func BenchmarkRoundtrip(b *testing.B) {
 	b.ReportAllocs()
 	for i := 0; i < b.N ; i++ {
 		builder.Reset()
-		ReadUser(MakeUser(builder, []byte("Arthur Dent"), 42))
+		buf := MakeUser(builder, []byte("Arthur Dent"), 42)
+		got_name, _ := ReadUser(buf)
+		if i == 0 {
+			b.SetBytes(int64(len(buf)))
+		}
+		// do some work to prevent cheating the benchmark:
+		bytes.Equal(got_name, []byte("Arthur Dent"))
 	}
 }
-
